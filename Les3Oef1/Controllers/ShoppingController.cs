eac02d3881db6b7928cdb695139351917a145e4c
RondelezLaura <laura.rondelez@student.howest.be>
2018-02-26 10:49:44 +0100

    deel1 models branch

---
+++ Les3Oef1/Controllers/ShoppingController.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Les3Oef1.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        private List<ShoppingList> items = new List<ShoppingList>();
+        public ActionResult Index()
+        {
+            if (TempData.Peek("items") != null)
+            {
+                items = JsonConvert.DeserializeObject<List<ShoppingList>>(TempData["items"].ToString());
+            }
+            return View(items);
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View(new ShoppingList { });
+        }
+
+        [HttpPost]
+        public ActionResult Create(ShoppingList item)
+        {
+            items.Add(item);
+            TempData["items"] = JsonConvert.SerializeObject(items);
+            return View("Finish", item);
+        }
+    }
+}
---
+++ Les3Oef1/Models/ShoppingList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Les3Oef1
+{
+    public class ShoppingList
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public int Aantal { get; set; }
+        
+    }
+}
